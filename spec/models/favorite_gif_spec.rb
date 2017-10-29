5de13f733e62727f1ff1b2f109238cad71d4acd8
Emily Wise <wise.m.emily@gmail.com>
2017-10-29 15:51:05 -0600

    Tests for Favoritegif model

--- spec/models/favorite_gif_spec.rb
+++ spec/models/favorite_gif_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe FavoriteGif, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a user and a gif id" do
+        user = User.create(username: "Emily", email: "Emily@gmail.com", password: "123")
+
+        favorite_gif= user.favorite_gifs.new
+
+        expect(favorite_gif).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with content" do
+        user = User.create(username: "Emily", email: "Emily@gmail.com", password: "123")
+        category = Category.create(name: "Monday")
+        gif = Gif.create(image_path: "https://media2.giphy.com/media/gEtjpEXi4luLe/giphy.gif", category: category)
+        favorite_gif= user.favorite_gifs.create(gif_id: gif.id)
+
+        expect(favorite_gif).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "belongs to user" do
+      user = User.create(username: "Emily", email: "Emily@gmail.com", password: "123")
+      category = Category.create(name: "Monday")
+      gif = Gif.create(image_path: "https://media2.giphy.com/media/gEtjpEXi4luLe/giphy.gif", category: category)
+      favorite_gif= user.favorite_gifs.create(gif_id: gif.id)
+      expect(favorite_gif).to respond_to(:user)
+    end
+
+  it "belongs to gif" do
+    user = User.create(username: "Emily", email: "Emily@gmail.com", password: "123")
+    category = Category.create(name: "Monday")
+    gif = Gif.create(image_path: "https://media2.giphy.com/media/gEtjpEXi4luLe/giphy.gif", category: category)
+    favorite_gif= user.favorite_gifs.create(gif_id: gif.id)
+    expect(favorite_gif).to respond_to(:gif)
+    end
+  end
 end
