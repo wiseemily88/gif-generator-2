8373cb2d4e6f9e025b7170e94b26ab82efbb95f1
Emily Wise <wise.m.emily@gmail.com>
2017-10-29 12:18:47 -0600

    updated index test

--- spec/features/user_can_mark_a_gif_a_favorite_spec.rb
+++ spec/features/user_can_mark_a_gif_a_favorite_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 describe "a user can favorite a gif" do
   scenario "a user is able to click on favorite button for a gif from the index page" do
-    admin_user= User.create(username: "admin", password: "pass", email: "admin@gmail.com", role: 1)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
+    user= User.create(username: "admin", password: "pass", email: "admin@gmail.com", role: 0)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     attributes = attributes_for(:category)
+    category_1 = Category.create!(name: "Monday")
+    category_2 = Category.create!(name: "Puppy")
 
-    visit user_gifs_path(admin_user)
+    gif_1 = category_1.gifs.create!(image_path: "http://gph.is/2xMJrRb")
+    gif_2 = category_1.gifs.create!(image_path: "http://gph.is/1L8sDoZ")
+    gif_3 = category_1.gifs.create!(image_path: "http://gph.is/1PFOSyk")
+    gif_4 = category_2.gifs.create!(image_path: "http://gph.is/29dwQaU")
 
+    visit user_gifs_path(user)
+    save_and_open_page
 
-    click_button  "favorite"
+    first(:button, 'Favorite' ).click
 
-    expect(current_path).to eq(user_favorite_gifs_path(admin_user))
+    expect(current_path).to eq(user_favorite_gifs_path(user))
 
   end
 end
