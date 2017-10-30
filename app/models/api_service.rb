bbb39e912f01dcc8391f4f2995ee0f01614e9735
Emily Wise <wise.m.emily@gmail.com>
2017-10-30 11:58:10 -0600

    updated key

--- app/models/api_service.rb
+++ app/models/api_service.rb
@@ -5,7 +5,8 @@ require 'pry'
 class ApiService
 
   def initialize(category)
-    key = open('app/assets/.api_key').read()
+    key = ENV['giphy_key']
+    #  open('app/assets/.api_key').read()
     url = "https://api.giphy.com/v1/gifs/search?api_key=#{key}&q=#{category}&limit=5&offset=0&rating=G&lang=en"
     uri = URI(url)
     response = Net::HTTP.get(uri)
